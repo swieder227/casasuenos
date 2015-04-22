18bd22bd1e2abdfbea75e738dfaf88f3d82a8d41
Seth Wieder <swieder227@gmail.com>
2015-04-21 22:21:03 -0400

    initial commit. underscores. theme started

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package casasuenos
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'casasuenos' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-header-banner">
+			<div class="site-branding">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" aria-label="<?php bloginfo( 'name' ); ?>">
+					<div class="site-logo"></div>
+				</a>
+			</div><!-- .site-branding -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Menu', 'casasuenos' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package casasuenos
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main container-fluid" role="main">
+
+		<?php
+
+			// Get Pages, sort by order
+			$pages = get_pages( array('sort_column' => 'menu_order'));
+			foreach ($pages as $page_data) {
+
+				// ACF Object (advanced custom fields)
+				$fields = get_fields($page_data);
+				// Boolean to show/hide on index.php
+				if($fields['show_on_home_page']){
+
+					// Get content
+					$page_title = $page_data->post_title;
+					$page_home_title = $fields['home_title'];
+					$page_home_paragraph = $fields['home_paragraph'];
+					$page_home_anchor = $fields['home_button'];
+					$page_url = get_permalink($page_data->ID);
+
+					// div#title view with content.
+					echo "<div id=".$page_title." class='home-row'>";
+						echo "<div class='home-bg'></div>";
+						echo "<div class='home-col'>";
+							// Titles. h2{1st word} + h3{rest of string}
+							echo "<h2 class='home-header2 font-sacramento'>".explode(' ',trim($page_home_title))[0]."</h2>";
+							if(strstr($page_home_title," "))
+							echo "<h3 class='home-header3'>".strstr($page_home_title," ")."</h3>";
+							echo "<p>".$page_home_paragraph."</p>";
+							if($page_home_anchor)
+								echo "<a class='home-anchor font-sacramento' href='".$page_url."'>".$page_home_anchor."</a>";
+						echo "</div>";
+					echo "</div>";
+
+				}
+
+			}
+
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
