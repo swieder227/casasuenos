6f086526794bb2039cf295f2a956d4de2fd7edb7
Seth Wieder <swieder227@gmail.com>
2015-05-06 23:27:49 -0400

    added activities page

---
+++ page-activities.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: Activities Page
+ *
+ *
+ *
+ * @package casasuenos
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+
+		<div class="page-topimg page-topimg--active">
+			<div class="toptabs-contain">
+				<div class="toptabs">
+					<div class="toptab font-sacramento"><a href="#">activities</a></div>
+				</div>
+			</div>
+		</div>
+
+		<main id="main" class="site-main" role="main">
+
+			<div class="border-wave"></div>
+
+			<div class="casa-row casa-row--nopad">
+				<div class="casa-col casa-col--text clear casa-col--100p">
+					<p class="body-text body-text--active">
+						Welcome to the spa in Casa del los Sueños. Our beautiful, peaceful spa gives you the special attention and treatment you deserve. With the concept of promoting true pampering and special attention in a beautiful and peaceful atmosphere, we are proud to give you an opportunity to immerse yourself our enchanting treatments. Our spa offers a serene and exclusive environment for a wide range of relaxing and rejuvenating treatments. Our well trained and skilled therapists will leave you with another memorable experience of Isla Mujeres, where you can experience fabulous delights for the mind, body and spirit.
+					</p>
+					<p class="body-text body-text--active">
+						<?php $email = get_fields( get_page( 1729 ) )['email_contact'] ?>
+
+						<b>For reservations:</b> <?php echo "<a class='' href='$email' target='_blank'>$email</a>" ?>
+					</p>
+				</div>
+			</div>
+
+
+			<div class="border-wave"></div>
+			<div class="casa-row clear casa-row--active1">
+			    <div class="casa-col--bg"></div>
+			    <div class="casa-col--text casa-col--pattern-yellow clear">
+			        <h2 class="home-header2 font-sacramento">Bikes</h2>
+			        <p class="home-paragraph">Grab one of the Casa bikes and explore the island.</p>
+			    </div>
+			</div>
+			<div class="border-wave"></div>
+			<div class="casa-row clear casa-row--active2">
+			    <div class="casa-col--bg"></div>
+			    <div class="casa-col--text casa-col--right casa-col--pattern-blue clear">
+			        <h2 class="home-header2 font-sacramento">Pool</h2>
+			        <p class="home-paragraph">Swim or relax by our gorgeous infinity pool, just for guests.</p>
+			    </div>
+			</div>
+			<div class="border-wave"></div>
+			<div class="casa-row clear casa-row--active3">
+			    <div class="casa-col--bg"></div>
+			    <div class="casa-col--text casa-col--pattern-blue clear">
+			        <h2 class="home-header2 font-sacramento">Parque<span class="home-header2-sm">de los</span>Suenos</h2>
+			        <p class="home-paragraph">Ready for some fun? The Casa’s adventurous-side is Parque de los Suenos. Located on the lower part of the property, head to the Parque for island adventure.</p>
+			        <a class="home-anchor font-sacramento" href="#">see more</a>
+			    </div>
+			</div>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
--- page-transportation.php
+++ page-transportation.php
@@ -32,9 +32,9 @@ get_header(); ?>
 					<p class="body-text body-text-title">Round Trip Gold Transportation</p>
 					<p class="body-text body-text-sm">Upon arrival to the Cancun International airport, our Gold transportation guests will be greeted by a travel agent holding a place card with their name.
 						Guests will be transported directly to the Ferry, where the agent will give you round trip tickets.  Please put these tickets in a safe place, for your return trip.  The trip over is 15 minutes.<br/><br/>Once you arrive, the taxi station is next door, the guests simply need to walk over and indicate to the driver that their destination is "Casa de Los Sueños",  the drivers know the location.  Our hotel will pay this fee once you arrive.   The same procedure  is followed for your for return trip.</p>
-					<p class="body-text body-text-subtitle">Gold Round Trip Transportation:   $100 USD per person
+					<p class="body-text body-text-subtitle">Gold Round Trip Transportation: $100 USD per person
 					<br>
-						Gold One Way Transportation:       $75 USD per person</p>
+						Gold One Way Transportation: $75 USD per person</p>
 					<p class="body-text body-text-title">Round Trip Platinum Transportation</p>
 					<p class="body-text body-text-sm">At Cancun airport, our Platinum Transportation Guests will be greeted by a travel agent holding a place card with their name. They will proceed to a private yacht where they will be transported in first class style directly to the pier of Casa de los Sueños.</p>
 					<p class="body-text body-text-subtitle">Platinum Round Trip Transportation:  $800 USD round trip</p>
